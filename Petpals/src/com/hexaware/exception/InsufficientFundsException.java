86c4b8007a737d1e0986d5ad2e08ed690b16a595
lokeshwarancse <119004634+lokeshwarancse@users.noreply.github.com>
2024-03-25 15:29:52 +0530

    Add files via upload

---
+++ Petpals/src/com/hexaware/exception/InsufficientFundsException.java
@@ -0,0 +1,19 @@
+package com.hexaware.exception;
+
+
+public class InsufficientFundsException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private String message;
+
+	public InsufficientFundsException(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+}
+
+
---
+++ Petpals/src/com/hexaware/util/DBUtil.java
@@ -0,0 +1,28 @@
+package com.hexaware.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+	static Connection con;
+	public static Connection getDBConn(){
+		try {
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/codechal","root","lokesh1908");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			
+		}
+		return con;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(getDBConn());
+	}
+
+}
+
